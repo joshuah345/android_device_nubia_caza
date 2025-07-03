b38cc8270b3f7c841b7eeef7f60e7f1db8343b63
hiroshi <hiroshi@tanuki.page>
2025-07-03 16:05:21 -0400

    tiro: remove blob fixup for sensors.hal.tof.so
    Not needed since tiro doesn't have/use this blob.

--- extract-files.py
+++ extract-files.py
@@ -175,8 +175,6 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libhidlbase_shim.so'),
     'vendor/lib64/libril-db.so': blob_fixup()
         .binary_regex_replace(rb'persist\.vendor\.radio\.poweron_opt', rb'persist.vendor.radio.poweron_ign'),
-    'vendor/lib64/hw/sensors.hal.tof.so': blob_fixup()
-        .binary_regex_replace(b'\x00input\x00', b'\x00fakei\x00'),
     'vendor/lib64/libNubiaImageAlgorithmVD.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
