8314020da6448cac2c3ad151dbab03ad3273b04f
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-03 11:57:38 -0400

    tiro: Use tiro diff kernel config instead of cerro

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,7 @@ TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/pineapple_GKI.config \
     oem/pineapple_diff.config \
-    oem/boards/cerro_diff.config
+    oem/boards/tiro_diff.config
 
 TARGET_KERNEL_SOURCE := kernel/nubia/sm8650
 TARGET_KERNEL_EXT_MODULE_ROOT := kernel/nubia/sm8650-modules
