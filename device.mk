d6770590020bad9f90014d70b4e5e53380311c2d
hiroshi <hiroshi@tanuki.page>
2025-07-03 12:14:11 -0400

    tiro: drop display config from cerro

--- device.mk
+++ device.mk
@@ -121,8 +121,7 @@ PRODUCT_PACKAGES += \
     libgpu_tonemapper
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/display_id_4630946566460097411.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946566460097411.xml \
-    $(DEVICE_PATH)/configs/display_id_4630947039571902851.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947039571902851.xml
+    $(DEVICE_PATH)/configs/display_id_4630946566460097411.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946566460097411.xml
 
 # DRM
 PRODUCT_PACKAGES += \
